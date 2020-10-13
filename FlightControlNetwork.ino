43d79bdf0ac6a7ad6b54a93d971ac69a2854c902
eronth <eronth@gmail.com>
2020-10-13 15:41:59 -0500

    Final throttle controls and buttons.

--- FlightControlNetwork.ino
+++ FlightControlNetwork.ino
@@ -16,12 +16,11 @@ https://github.com/MHeironimus/ArduinoJoystickLibrary */
 
 // Create Joystick
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
-  JOYSTICK_TYPE_MULTI_AXIS, 0, 0,
+  JOYSTICK_TYPE_MULTI_AXIS, 7, 0,
   false, false, false, false, false, false,
   true, true, false, false, false);
 
-// Digital input pin setup:
-
+// Digital/Analog input pin setup:
 const int rudderEncoder1Pin = 0; // Rudder
 const int rudderEncoder2Pin = 1; // Rudder
 const int rudderButtonPin = 2;
@@ -56,20 +55,44 @@ const int throttleRangeMax = 127;
 char throttlePos = initialPos;
 int throttleToggleF;
 int throttleToggleB;
-int throttleUpDownState = 0;
-int throttleButton1State;
-int throttleButton2State;
-int throttleButton3State;
-int throttleButton4State;
+int throttleToggleFullState;
+int throttleToggleLastFullState;
+int throttleToggleFStateButtonMap = 4;
+int throttleToggleNStateButtonMap = 5;
+int throttleToggleBStateButtonMap = 6;
 
+int throttleUpDownState = 0;
+int throttleButton1State = 0;
+int throttleButton2State = 0;
+int throttleButton3State = 0;
+int throttleButton4State = 0;
+int throttleButton1LastState = 0;
+int throttleButton2LastState = 0;
+int throttleButton3LastState = 0;
+int throttleButton4LastState = 0;
+const int throttleButton1JSB = 0;
+const int throttleButton2JSB = 1;
+const int throttleButton3JSB = 2;
+const int throttleButton4JSB = 3;
+const int throttleToggleF_JSB = 4;
+const int throttleToggleN_JSB = 5;
+const int throttleToggleB_JSB = 6;
+int throttleToggleCountDelay;
+
+
+// Placeholder
 int pin5State;
-  int pin6State;
-  int pin7State;
+int pin6State;
+int pin7State;
+
 
 // Loop check for debugging. Only iterates loops on changes.
 int loopN = 1;
+bool pressed = false;
 
-
+//////////////////////////////////////////////////////////////////////////////////////
+//                                    SETUP
+//////////////////////////////////////////////////////////////////////////////////////
 // The setup routine runs once when you first add power or press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -84,10 +107,10 @@ void setup() {
   pinMode(inPin5, INPUT_PULLUP );
   pinMode(inPin6, INPUT_PULLUP );
   pinMode(inPin7, INPUT);
-  pinMode(throttleButton1Pin, INPUT);
-  pinMode(throttleButton2Pin, INPUT);
-  pinMode(throttleButton3Pin, INPUT);
-  pinMode(throttleButton4Pin, INPUT);
+  pinMode(throttleButton1Pin, INPUT_PULLUP);
+  pinMode(throttleButton2Pin, INPUT_PULLUP);
+  pinMode(throttleButton3Pin, INPUT_PULLUP);
+  pinMode(throttleButton4Pin, INPUT_PULLUP);
   // Copied code!!!!!
   Serial.println(F("Initialize System"));
   //Init potentiometer
@@ -114,6 +137,9 @@ void setup() {
   Serial.println("End of Initialize.");
 }
 
+//////////////////////////////////////////////////////////////////////////////////////
+//                                    REGULAR LOOP
+//////////////////////////////////////////////////////////////////////////////////////
 // The loop routine runs over and over again forever:
 void loop() {
   // Read the input pins:
@@ -137,11 +163,61 @@ void loop() {
 
   //readPot();
 
-  // Read throttle inputs
-  // The max and min seems to be... 919, 112
+  //------------------------------------------------------------------------------------
+  //                                    Throttle Inputs
+  //------------------------------------------------------------------------------------
+  // Throttle Button Presses
+  if (throttleButton1State != throttleButton1LastState) {
+    Joystick.setButton(throttleButton1JSB, throttleButton1State);
+    throttleButton1LastState = throttleButton1State;
+    pressed = !pressed;
+  }
+  if (throttleButton2State != throttleButton2LastState) {
+    Joystick.setButton(throttleButton2JSB, throttleButton2State);
+    throttleButton2LastState = throttleButton2State;
+    pressed = !pressed;
+  }
+  if (throttleButton3State != throttleButton3LastState) {
+    Joystick.setButton(throttleButton3JSB, throttleButton3State);
+    throttleButton3LastState = throttleButton3State;
+    pressed = !pressed;
+  }
+  if (throttleButton4State != throttleButton4LastState) {
+    Joystick.setButton(throttleButton4JSB, throttleButton4State);
+    throttleButton4LastState = throttleButton4State;
+    pressed = !pressed;
+  }
+  
+  // Throttle Toggle Inputs
+  if (throttleToggleF == 1) {
+    throttleToggleFullState = 1;
+  } else if (throttleToggleB == 1) {
+    throttleToggleFullState = -1;
+  } else {
+    throttleToggleFullState = 0; 
+  }
+  // If the state has changed, we need to toggle a quick button press.
+  if (throttleToggleLastFullState != throttleToggleFullState) {
+    if (throttleToggleFullState == -1) {
+      Joystick.setButton(throttleToggleB_JSB, 1);
+    } else if (throttleToggleFullState == 0) {
+      Joystick.setButton(throttleToggleN_JSB, 1);
+    } else if (throttleToggleFullState == 1) {
+      Joystick.setButton(throttleToggleF_JSB, 1);
+    }
+    throttleToggleLastFullState = throttleToggleFullState;
+  } else {
+    Joystick.setButton(throttleToggleF_JSB, 0);
+    Joystick.setButton(throttleToggleN_JSB, 0);
+    Joystick.setButton(throttleToggleB_JSB, 0);
+  }
+
+  // Throttle up/down input.
+  // The max and min seems to be... 919, 112, and I don't know why!
   int throttledUp = 112;
   int throttledDown = 919;
   if (throttleToggleF == 1 || throttleToggleB == 1) {
+    
     // for some reason, 924 and 153 if shields up
     throttledUp = 153;
     throttledDown = 924;
@@ -149,7 +225,9 @@ void loop() {
   int throttleMap = map(throttleUpDownState, throttledDown, throttledUp, throttleRangeMin, throttleRangeMax);
   Joystick.setThrottle(throttleMap);
 
-  // Read rudder inputs
+  //------------------------------------------------------------------------------------
+  //                                    Rudder inputs
+  //------------------------------------------------------------------------------------
   if (rudderEncoder1State != rudderEncoder1LastState) {
     if (rudderEncoder2State != rudderEncoder1State) {
       RudderPos++;
@@ -162,25 +240,29 @@ void loop() {
     }
     rudderEncoder1LastState = rudderEncoder1State;
   }
+  
+  //------------------------------------------------------------------------------------
+  //                                    Output/Debug section
+  //------------------------------------------------------------------------------------
   outputDebugValues();
-  // String output;
-    String output = String("|") 
-                    + " Position:" + displaySpacing((int)RudderPos, 3, true)
-                    + "  |  Set number:" + displaySpacing((int)loopN, 5, false)
-                    + "  |  (" + rudderEncoder1State + ", " + rudderEncoder2State + ")"
-                    + "  | Rudder Click: " + rudderButtonPress
-                    + "  |  Throttle Toggle: " + throttleToggleF + " " + throttleToggleB
-                    + "  |  Throttled to " + throttleUpDownState + " " + throttleMap
-                    + "  |  Buttons: " + throttleButton1State + " " + throttleButton2State + " " + throttleButton3State + " " + throttleButton4State
-                    + "  |  p6: " + pin6State
-                    + "  |  p7: " + pin7State
+  String output = String("|") 
+                  + " Position:" + displaySpacing((int)RudderPos, 3, true)
+                  + "  |  Set number:" + displaySpacing((int)loopN, 5, false)
+                  + "  |  (" + rudderEncoder1State + ", " + rudderEncoder2State + ")"
+                  + "  | Rudder Click: " + rudderButtonPress
+                  + "  |  Throttle Toggle: " + displaySpacing((int)throttleToggleFullState, 1, true) + " from " + throttleToggleF + " " + throttleToggleB
+                  + "  |  Throttled to " + throttleUpDownState + " " + throttleMap
+                  + "  |  Buttons: " + pressed + throttleButton1State + " " + throttleButton2State + " " + throttleButton3State + " " + throttleButton4State
+                  //+ "  |  p6: " + pin6State
+                  //+ "  |  p7: " + pin7State
+                  + "  |";
                   
-                    + "  |";
-    loopN++;
-    // Print out the state of the button:
-    Serial.println(output);
-
-  delay(1);        // delay in between reads for stability
+  loopN++;
+  // Print out the state of the button:
+  Serial.println(output);
+  
+  // delay in between reads for stability
+  delay(1);        
 }
 
 void outputDebugValues()
@@ -205,6 +287,7 @@ float phys = ( 1/*R*/ * (5 - Vout))/5;//Conversion voltage to resistance
 }
 ///////////////////////////////////// COPIED CODE ////////////////////////////
 
+// Function to cleanly handle the spacing for debug output.
 String displaySpacing(int valueToSpaceFor, int maxDigits, bool canBeNegative)
 {
   String retval;
